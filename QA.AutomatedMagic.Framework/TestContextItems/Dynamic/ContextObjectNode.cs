fa3056f72107d16d28778cbea064b87d23244132
Kiryl_Sakalouski <kiryl_sakalouski@epam.com>
2016-03-16 20:22:38 +0300

    Add MagicServer

---
+++ QA.AutomatedMagic.Framework/TestContextItems/Dynamic/ContextObjectNode.cs
@@ -0,0 +1,21 @@
+﻿namespace QA.AutomatedMagic.Framework.TestContextItems.Dynamic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MetaMagic;
+
+    [MetaType("Context object node")]
+    public class ContextObjectNode : BaseDynamicNode
+    {
+        [MetaTypeValue("Path to object in context")]
+        public string Path { get; set; }
+
+        public override object GetValue(TestContext context)
+        {
+            return MetaType.SerializeObject(context.ResolveValue(Path));
+        }
+    }
+}
---
+++ QA.AutomatedMagic.MagicServer/Models/MagicPickerItem.cs
@@ -0,0 +1,13 @@
+﻿namespace QA.AutomatedMagic.MagicServer.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class MagicPickerItem
+    {
+        public bool NeedToAdd { get; set; }
+        public string Name { get; set; }
+    }
+}
