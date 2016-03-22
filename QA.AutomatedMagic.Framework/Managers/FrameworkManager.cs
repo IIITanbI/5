f56d0e1e0f6074257b9a67f9048c73a444d0ad07
Kiryl_Sakalouski <kiryl_sakalouski@epam.com>
2016-03-22 18:25:42 +0300

    Added Sleeeeeeeep

---
+++ QA.AutomatedMagic.Framework/Managers/FrameworkManager.cs
@@ -0,0 +1,21 @@
+﻿namespace QA.AutomatedMagic.Framework.Managers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CommandsMagic;
+    using System.Threading;
+
+    [CommandManager("FrameworkManager")]
+    public class FrameworkManager : BaseCommandManager
+    {
+        [Command("Sleep")]
+        public void Sleep(string seconds, ILogger log)
+        {
+            log?.INFO($"Sleep for: {seconds} seconds");
+            Thread.Sleep(int.Parse(seconds) * 1000);
+        }
+    }
+}
