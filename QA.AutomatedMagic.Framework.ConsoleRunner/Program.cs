13956f9a0c36f84ed90d5c6fcee69c334cb9ac63
Kiryl_Sakalouski <kiryl_sakalouski@epam.com>
2016-03-28 12:49:14 +0300

    added file logger

--- QA.AutomatedMagic.Framework.ConsoleRunner/Program.cs
+++ QA.AutomatedMagic.Framework.ConsoleRunner/Program.cs
@@ -10,7 +10,7 @@
     using System.IO;
     using System.Reflection;
     using Reports.HtmlReport;
-
+    using TestInfo;
     class Program
     {
         static string _libPath = "Libs";
@@ -40,13 +40,24 @@
                 }
 
                 AutomatedMagicManager.LoadAssemblies(_libPath, SearchOption.AllDirectories);
-
+                TestManager.Log.AddLogger(new FileLogger("Log\\ManagerLog.txt"), LogLevel.TRACE);
                 TestManager.Log.INFO(AutomatedMagicManager.GetReflectionInfo());
+
 
                 var projectConfig = XDocument.Load(runConfig.PathToProjectConfig);
                 var project = MetaType.Parse<TestProject>(projectConfig.Elements().First());
                 project.Build();
-                project.Execute();
+
+                try
+                {
+                    project.Execute();
+                }
+                catch (Exception ex)
+                {
+                    TestManager.Log.ERROR("Error occurred during project executing", ex);
+                }
+
+                var ts = AutomatedMagicManager.LoadedMetaTypes;
 
                 var result = project.GetTestInfo();
                 var xel = MetaType.SerializeObject(result) as XElement;
@@ -81,6 +92,90 @@
 
                 if (!File.Exists(newPath))
                     file.CopyTo(newPath);
+            }
+        }
+
+        public class FileLogger : ILogger
+        {
+            private string _fileName;
+            private object _lock = new object();
+
+            public FileLogger(string fileName)
+            {
+                _fileName = fileName;
+            }
+            public void AddLogger(ILogger logger, LogLevel level)
+            {
+            }
+
+            #region Simple actions
+            public void TRACE(string message, Exception exception = null)
+            {
+                LOG(LogLevel.TRACE, message, exception);
+            }
+            public void TRACE(string message, LoggedFileType fileType, string filePath)
+            {
+                LOG(LogLevel.TRACE, message, fileType, filePath);
+            }
+
+            public void DEBUG(string message, Exception exception = null)
+            {
+                LOG(LogLevel.DEBUG, message, exception);
+            }
+            public void DEBUG(string message, LoggedFileType fileType, string filePath)
+            {
+                LOG(LogLevel.DEBUG, message, fileType, filePath);
+            }
+
+            public void WARN(string message, Exception exception = null)
+            {
+                LOG(LogLevel.WARN, message, exception);
+            }
+            public void WARN(string message, LoggedFileType fileType, string filePath)
+            {
+                LOG(LogLevel.WARN, message, fileType, filePath);
+            }
+
+            public void INFO(string message, Exception exception = null)
+            {
+                LOG(LogLevel.INFO, message, exception);
+            }
+            public void INFO(string message, LoggedFileType fileType, string filePath)
+            {
+                LOG(LogLevel.INFO, message, fileType, filePath);
+            }
+
+            public void ERROR(string message, Exception exception = null)
+            {
+                LOG(LogLevel.ERROR, message, exception);
+            }
+            public void ERROR(string message, LoggedFileType fileType, string filePath)
+            {
+                LOG(LogLevel.ERROR, message, fileType, filePath);
+            }
+            #endregion
+
+            public void LOG(LogLevel level, string message, Exception exception = null)
+            {
+                lock (_lock)
+                {
+                    using (var sw = new StreamWriter(_fileName, true))
+                    {
+                        if (exception == null)
+                            sw.WriteLine($"{level}\t{message}");
+                        else
+                            sw.WriteLine($"{level}\t{message}\nException:\n{exception}");
+                        sw.Flush();
+                    }
+                }
+            }
+            public void LOG(LogLevel level, string message, LoggedFileType fileType, string filePath)
+            {
+            }
+
+            public string GetLoggedFilesFolder()
+            {
+                return null;
             }
         }
     }
--- QA.AutomatedMagic/TestInfo/TestEnvironmentInfo.cs
+++ QA.AutomatedMagic/TestInfo/TestEnvironmentInfo.cs
@@ -1,25 +1,34 @@
 ﻿namespace QA.AutomatedMagic.TestInfo
 {
+    using MetaMagic;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    public class TestEnvironmentInfo
+    [MetaType("TestEnvironmentInfo")]
+    public class TestEnvironmentInfo : BaseMetaObject
     {
+        [MetaTypeValue("CLRVersion", IsRequired = false)]
         public string CLRVersion { get; set; }
 
+        [MetaTypeValue("OSName", IsRequired = false)]
         public string OSName { get; set; }
 
+        [MetaTypeValue("OSVersion", IsRequired = false)]
         public string OSVersion { get; set; }
 
+        [MetaTypeValue("Platform", IsRequired = false)]
         public string Platform { get; set; }
 
+        [MetaTypeValue("MachineName", IsRequired = false)]
         public string MachineName { get; set; }
 
+        [MetaTypeValue("User", IsRequired = false)]
         public string User { get; set; }
 
+        [MetaTypeValue("UserDomain", IsRequired = false)]
         public string UserDomain { get; set; }
 
     }
